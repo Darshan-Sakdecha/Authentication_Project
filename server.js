43f3b89e32544afdbebe1f08e070cac39afb26d0
darshansakdecha <darshansakdecha@gmail.com>
2025-07-27 16:13:23 +0530

    complete Backend

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
+const allowOrigins = ['http://localhost:5173']
 
 //Request passed in json (postman thi jyare body thi data mokli e mate!!)
 //Cors is used to connect frontend & backend
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
+  origin:allowOrigins,
   credentials: true,
 }));
 
